fad208cfe126740e7bcd0cfec2d00b9a1d3819ac
Shivam Tiwari <shivamtiwari@singsys.com>
2025-04-26 08:31:47 +0530

    merge

--- lms_backend/server.js
+++ lms_backend/server.js
@@ -1,10 +1,19 @@
 import app from './src/app.js';
 import dotenv from 'dotenv'
+import sequelize from './src/config/db.js';
+
 dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port link${PORT}`);
-})
+const startServer = async () => {
+  try {
+      await sequelize.authenticate();
+      await sequelize.sync();
+      app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  } catch (error) {
+      process.exit(1);
+  }
+};
 
+startServer();
